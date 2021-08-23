50e4554918523544da8ebec066e66733c861b3a4
justin_k_m <justinm0623@gmail.com>
2021-08-23 18:50:09 -0400

    Paused state, working?

---
+++ Assets/Scripts/Player/PlayerPaused.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaused : PlayerState
+{
+    // Constructor
+    private Player playerRef;
+    private Main gameRef;
+    private PlayerState previousState;
+    public PlayerPaused(Player playerRef, Main gameRef, PlayerState previousState = null) : base(playerRef, gameRef)
+    {
+        this.playerRef = playerRef;
+        this.gameRef = gameRef;
+        this.previousState = previousState;
+    }
+
+    // Tick called every FixedUpdate in Player
+    public override void Tick()
+    {
+        // When the game unpauses, start the game
+        if (!gameRef.paused)
+        {
+            // If there is no previous state, set to normal, else use previous state
+            if (previousState == null) { playerRef.SetState(new PlayerNormal(playerRef, gameRef)); }
+            else { playerRef.SetState(previousState); }
+        }
+    }
+
+    // Called when entering state
+    public override void OnStateEnter()
+    {
+        playerRef._renderer.material.color = Color.gray;
+    }
+}
