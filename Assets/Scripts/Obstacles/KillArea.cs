e3d2459213ffd8cbfcf9694559332d1e98cf3e01
justin_k_m <justinm0623@gmail.com>
2021-08-27 19:04:26 -0400

    Update KillArea.cs
    Destroy function for Rage mode

--- Assets/Scripts/Obstacles/KillArea.cs
+++ Assets/Scripts/Obstacles/KillArea.cs
@@ -9,6 +9,32 @@ public class KillArea : Area
     [SerializeField]
     public Sprite[] sprites;
 
+    [Header("Destruction Sprite Name")]
+    [SerializeField]
+    private string desName;
+
+
+    private Sprite[] desSprites;
+    new private void Start()
+    {
+        base.Start();
+
+        desSprites = Resources.LoadAll<Sprite>("Sprites/Obstacles/Individual/" + desName + "/");
+
+        // 8 different particles
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject obj = Instantiate(Resources.Load<GameObject>("Prefabs/Particle System"), transform.Find("Particle System"));
+
+            Material material = new Material(Shader.Find("Standard"));
+            material.mainTexture = desSprites[i].texture;
+
+            obj.GetComponent<ParticleSystemRenderer>().material = material;
+        }
+    }
+
+
+
     private void FixedUpdate()
     {
         Move();
@@ -22,9 +48,33 @@ public class KillArea : Area
         transform.position = new Vector3(gameRef.GetComponent<Main>().teleportPoint, transform.position.y, transform.position.z);
     }
 
+    private void BreakObject()
+    {
+        // Disable sprite renderer
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        Transform t = transform.Find("Particle System");
+        foreach (Transform child in t)
+        {
+            child.GetComponent<ParticleSystem>().Play();
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.parent.name == "Player") { playerRef.GetComponent<Player>().isDamaged = true; } 
+        if (other.transform.parent.name == "Player") 
+        { 
+            if (playerRef.GetComponent<Player>().currentState.type != "PlayerRage")
+            {
+                playerRef.GetComponent<Player>().isDamaged = true;
+            }
+            else
+            {
+                BreakObject();
+            }
+        }
+            
     }
 
     new protected void OnDrawGizmos()
@@ -32,15 +82,17 @@ public class KillArea : Area
         Gizmos.color = new Color(1f, 0f, 0f, 1f);
         foreach (Transform child in transform)
         {
-            if (child.GetComponent<Collider>() as BoxCollider)
+            if (child.name.Contains("Collider"))
             {
-                Gizmos.DrawWireCube(child.GetComponent<BoxCollider>().bounds.center, child.GetComponent<BoxCollider>().bounds.size);
+                if (child.GetComponent<Collider>() as BoxCollider)
+                {
+                    Gizmos.DrawWireCube(child.GetComponent<BoxCollider>().bounds.center, child.GetComponent<BoxCollider>().bounds.size);
+                }
+                else if (child.GetComponent<Collider>() as SphereCollider)
+                {
+                    Gizmos.DrawWireSphere(child.GetComponent<SphereCollider>().bounds.center, child.GetComponent<SphereCollider>().radius);
+                }
             }
-            else if (child.GetComponent<Collider>() as SphereCollider)
-            {
-                Gizmos.DrawWireSphere(child.GetComponent<SphereCollider>().bounds.center, child.GetComponent<SphereCollider>().radius);
-            }
-            
         }
     }
 }
